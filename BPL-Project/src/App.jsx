6dfe7666c5d5a7f2051b623e1b3f91070ecbb058
shams al labib <samslabib19@gmail.com>
2025-09-22 02:40:55 +0600

    added cart section

--- BPL-Project/src/App.jsx
+++ BPL-Project/src/App.jsx
@@ -1,33 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { Suspense } from 'react'
 import './App.css'
+import Available from './components/Available Players/Available'
+import Navber from './components/Navber/Navber'
+import Selected from './components/Selected Players/selected'
+
+const fetchPlayers = async ()=>{
+  const res = await fetch('/players.json');
+  return res.json();
+}
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  // ekhane ekta kahini hosce ami jdi async await use kore data ta fetch kori tahole seta age call kore nite hobe, nahole sei data kaj korbena
+
+  const playersPromise = fetchPlayers();
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    {/* jate available and selected e 2 jaygatei button gula show kore ejonno amra main app.jsx ei korbo, tate kore page change holeo btn thakbe */}
+
+
+    
+    <Navber></Navber>
+    <Suspense fallback={<span className="loading loading-spinner loading-xl"></span>}>
+    <Available playersPromise={playersPromise}></Available>
+    </Suspense>
+    {/* <Selected></Selected> */}
     </>
   )
 }
---
+++ BPL-Project/src/components/Navber/Navber.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import navImg from '../../assets/logo.png'
+import dollarImg from '../../assets/dollar-1.png'
+
+
+const Navber = () => {
+    return (
+        <div>
+             <div className="navbar max-w-[1200px] mx-auto ">
+  <div className="flex-1">
+    <a className=" w-[60px] h-[60px] text-xl"><img src={navImg} alt="" /></a>
+  </div>
+  <div className="flex items-center">
+    <span className='mr-1'>60000</span>
+    <span className='mr-1'> Coin</span>
+    <img src={dollarImg} alt="" />
+  </div>
+</div>
+        </div>
+    );
+};
+
+export default Navber;
